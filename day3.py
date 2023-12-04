1189ee876edaa7135bf7cc7cdb93a3255a19335e
Ram Subramanian <ram@posteo.net>
2023-12-03 19:54:10 -0800

    day3: p2

--- day3.py
+++ day3.py
@@ -4,13 +4,13 @@ with open('day3.txt') as f:
 class Numeric:
     def __init__(self, num):
         self.num = num
-        self.marked = False
+        self.counted = False
     def __str__(self):
         return self.num
  
 nums = dict()
 syms = set()
-gears = set()
+gears = []
 total = 0
 
 for i, line in enumerate(lines):
@@ -25,20 +25,36 @@ for i, line in enumerate(lines):
         elif ch == '.':
             continue
         else:
+            t = set()
+            t.add((i-1, j-1))
+            t.add((i-1, j))
+            t.add((i-1, j+1))
+            t.add((i, j-1))
+            t.add((i, j+1))
+            t.add((i+1, j-1))
+            t.add((i+1, j))
+            t.add((i+1, j+1))
+            syms = syms.union(t)
             if ch == '*':
-                gears.add((i, j))
-            syms.add((i-1, j-1))
-            syms.add((i-1, j))
-            syms.add((i-1, j+1))
-            syms.add((i, j-1))
-            syms.add((i, j+1))
-            syms.add((i+1, j-1))
-            syms.add((i+1, j))
-            syms.add((i+1, j+1))
+                gears.append(t)
 
 for k in syms:
-    if k in nums and nums[k].marked == False:
+    if k in nums and not nums[k].counted:
         total += int(nums[k].num)
-        nums[k].marked = True
+        nums[k].counted = True
+
+print(total)
+
+total = 0
+for neighbors in gears:
+    ids = set()
+    for n in neighbors:
+        if n in nums:
+            ids.add(nums[n])
+    if len(ids) == 2:
+        t = 1
+        for x in ids:
+            t *= int(x.num) 
+        total += t
 
 print(total)
